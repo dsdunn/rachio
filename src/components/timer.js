bb60ff161fc031cd823931d3e27d4204f7f083c9
dsdunn <dsdunnmusic@gmail.com>
2018-10-14 12:13:32 -0600

    Add Timer file and skeleton

---
+++ src/components/timer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      timeLeft: 0
+    }
+  }
+
+  render() {
+    return (
+        <div className='timer'>
+          
+        </div>
+      )
+  }
+}
+export default Timer;
