f822275af3d8dac9660594cf2dd68ec44cfd0f5e
dsdunn <dsdunnmusic@gmail.com>
2018-10-14 10:18:04 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ]
+}
+
+
+
+
+
+
+
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+//     devtool: 'source-map',
+//     entry: './src/index.js',
+//     output: {
+//         path: path.join(__dirname, "/dist"),
+//         filename: "index_bundle.js"
+//     },
+//     module: {
+//         rules: [{
+//             test: /\.js$/,
+//             exclude: /node_modules/,
+//             use: {
+//                 loader: 'babel-loader'
+//             }
+//         }]
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({ template: "./src/index.html" })
+//     ]
+// }
+//         
