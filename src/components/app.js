d83a6fb9e1fa21ace5c6a2045e5f9e83ea6ecf32
dsdunn <dsdunnmusic@gmail.com>
2018-10-14 12:14:12 -0600

    add fetch file

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      allTimeLeft: 0,
+      allOn: false
+    }
+  }
   render() {
-    return (<h1>Rachio App</h1>)
+    return (
+      <div className='app'>
+        <h1>Rachio App</h1>
+        <section className='controls'>
+          <div className='all-zones-control'>
+          </div>
+        </section>
+      </div>
+      )
   }
 }
 
